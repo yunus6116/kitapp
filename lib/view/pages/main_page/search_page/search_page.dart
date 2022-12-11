552968e7c57c7b0b929fd9551ebed4d89f4c3b65
Yunus Emre Akyıldız <y.emreakyildiz53@gmail.com>
2022-12-11 17:16:05 +0300

    Changed from StreamBuilder to FutureBuilder

--- lib/view/pages/main_page/search_page/search_page.dart
+++ lib/view/pages/main_page/search_page/search_page.dart
@@ -35,123 +35,104 @@ class _SearchPageState extends State<SearchPage> {
             },
           ),
           Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream:
-                  FirebaseFirestore.instance.collection('books').snapshots(),
-              builder: (context, snapshots) {
-                if (snapshots.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (snapshots.hasData) {
-                  final ScrollController controllerOne = ScrollController();
-                  return CupertinoScrollbar(
-                    controller: controllerOne,
-                    thickness: 5.0,
-                    child: ListView.builder(
-                      controller: controllerOne,
-                      //itemCount: (searchItem.isEmpty) ? (snapshots.data!.docs.length) : 1,
-                      itemCount: snapshots.data?.docs.length,
-                      itemBuilder: (context, index) {
-                        var data = snapshots.data!.docs[index].data()
-                            as Map<String, dynamic>;
-                        if (searchItem.isEmpty) {
-                          return ListTile(
-                            title: Text(
-                              data['nameOfBook'],
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text(
-                              data['author'].toString(),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.normal),
-                            ),
-                            leading: CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(data['coverImageUrl']),
-                            ),
-                            trailing: const Icon(Icons.star_border_rounded,
-                                color: AppColors.primary),
-                          );
-                        } 
-                        else if (data['nameOfBook']
-                                .toString()
-                                .toLowerCase()
-                                .contains(searchItem.toLowerCase()) ||
-                            data['author']
-                                .toString()
-                                .toLowerCase()
-                                .contains(searchItem.toLowerCase())) {
-                          // return Card(
-                          //   child: Row(
-                          //     children: [
-                          //       Container(
-                          //           //height: 100,
-                          //           decoration: BoxDecoration(
-                          //             image: DecorationImage(
-                          //               image: NetworkImage(data['coverImageUrl']),
-                          //               fit: BoxFit.fitHeight
-                          //             ),
-                          //           ),
-                          //           ),
-                          //       SizedBox(
-                          //         width: 400,
-                          //         height: 100,
-                          //         child: ListTile(
-                          //           title: Text(data['nameOfBook']),
-                          //           subtitle: Text(data['author']),
-                          //           trailing: Icon(Icons.star_border_rounded),//Icons.star_rate_rounded
-                          //         ),
-                          //       ),
-                          //     ],
-                          //   ),
-                          // );
-                          return ListTile(
-                            title: Text(
-                              data['nameOfBook'],
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text(
-                              data['author'].toString(),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.normal),
-                            ),
-                            leading: CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(data['coverImageUrl']),
-                            ),
-                            trailing: const Icon(Icons.star_border_rounded,
-                                color:
-                                    AppColors.primary), //Icons.star_rate_rounded
-                          );
-                        } else {
-                          //return const Text('Cannot found what you are searching for :(', textAlign: TextAlign.center,);
-                          return Container();
-                        }
-                      },
-                    ),
-                  );
-                } else {
-                  return const Text("streamde data yok");
+            child: FutureBuilder<dynamic>(
+              future: FirebaseFirestore.instance.collection('books').get(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                //If snapshot has error
+                if (snapshot.hasError) {
+                  return const Text('Snapshot has error!');
                 }
+
+                //If connection is provided
+                if (snapshot.connectionState == ConnectionState.done) {
+                  //If data and document exist
+                  if (snapshot.hasData) {
+                    final ScrollController controllerOne = ScrollController();
+                    return CupertinoScrollbar(
+                      controller: controllerOne,
+                      thickness: 5.0,
+                      child: ListView.builder(
+                        controller: controllerOne,
+                        itemCount: snapshot.data?.docs.length,
+                        itemBuilder: (context, index) {
+                          var data = snapshot.data!.docs[index].data()
+                              as Map<String, dynamic>;
+
+                          //User has not typed yet -> (TEMPORARY) Show all / () Show categories
+                          if (searchItem.isEmpty) {
+                            return ListTile(
+                              title: Text(
+                                data['nameOfBook'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                data['author'].toString(),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(data['coverImageUrl']),
+                              ),
+                            );
+                          }
+
+                          //User is typing
+                          else if (data['nameOfBook']
+                                  .toString()
+                                  .toLowerCase()
+                                  .contains(searchItem.toLowerCase()) ||
+                              data['author']
+                                  .toString()
+                                  .toLowerCase()
+                                  .contains(searchItem.toLowerCase())) {
+                            return ListTile(
+                              title: Text(
+                                data['nameOfBook'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                data['author'].toString(),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(data['coverImageUrl']),
+                              ),
+                            );
+                          }
+
+                          //For the books/authors that does not match with searchItem
+                          else {
+                            return Container();
+                          }
+                        },
+                      ),
+                    );
+                  }
+                }
+
+                //If connection is waiting
+                return const Center(child: CircularProgressIndicator());
               },
             ),
           ),
