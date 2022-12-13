587b7390c8d73d481b2210081050fbb37fc2af04
Halil Emin Çalışkan <halileminclskn@gmail.com>
2022-12-13 18:16:37 +0300

    New profile buttons added
    -Change Pasword and Favourite books buttons added
    -Icons added to buttons

--- lib/view/pages/main_page/my_profile_page/my_profile_page.dart
+++ lib/view/pages/main_page/my_profile_page/my_profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kitapp/view/pages/main_page/my_profile_page/dump_data.dart';
-import 'package:kitapp/view/shared/widgets/favorite_books.dart';
 import 'package:kitapp/view/shared/widgets/profile.dart';
 
 import '../../../shared/styles/colors.dart';
@@ -17,41 +17,54 @@ class MyProfilePage extends HookConsumerWidget {
     const user = UserPreferences.myUser;
 
     return Scaffold(
-      appBar: CustomAppbar(
-        title: 'My Profile Page',
-        showBackButton: false,
-      ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          ProfileWidget(
-            onClicked: () async {},
-          ),
-          const SizedBox(height: 24),
-          buildName(user),
-          FavoriteBooksWidget(),
-          const SizedBox(height: 36),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  CustomButton(
-                    buttonText: 'Log out',
-                    backgroundColor: AppColors.primary,
-                    height: 55,
-                    borderRadius: 11,
-                    width: MediaQuery.of(context).size.width - 40,
-                    onPressed: () async {
-                      await ref.read(myProfileVMProvider).logout();
-                    },
-                  ),
-                ],
-              )),
-        ],
-      ),
-    );
+        appBar: CustomAppbar(
+          title: 'My Profile Page',
+          showBackButton: false,
+        ),
+        body: Column(
+          children: [
+            ProfileWidget(
+              onClicked: () async {},
+            ),
+            const SizedBox(height: 24),
+            buildName(user),
+            const SizedBox(height: 30),
+            buildButton("My Favourite Books", "profile_favourite", context),
+            const Divider(
+              color: Color(0xff4D506C),
+              indent: 20,
+              endIndent: 20,
+              thickness: 0.1,
+            ),
+            buildButton("Change Password", "profile_change_password", context),
+            Expanded(
+                child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CustomButton(
+                        icon: SvgPicture.asset('assets/svg/profile_logout.svg'),
+                        buttonText: 'Logout',
+                        backgroundColor: AppColors.primary,
+                        height: 55,
+                        borderRadius: 11,
+                        width: MediaQuery.of(context).size.width - 40,
+                        onPressed: () async {
+                          await ref.read(myProfileVMProvider).logout();
+                        },
+                      ),
+                    ],
+                  )),
+            )),
+            const SizedBox(
+              height: 15,
+            )
+          ],
+        ));
   }
 }
 
@@ -67,6 +80,26 @@ Widget buildName(User user) => Column(
         Text(
           user.email,
           style: const TextStyle(fontSize: 13, color: Color(0xff4D506C)),
+        ),
+      ],
+    );
+
+Widget buildButton(String title, String iconName, BuildContext context) =>
+    Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomButton(
+          icon: SvgPicture.asset("assets/svg/$iconName.svg"),
+          buttonText: title,
+          backgroundColor: Colors.white,
+          textColor: Color(0xff4D506C),
+          height: 55,
+          borderRadius: 11,
+          width: MediaQuery.of(context).size.width - 40,
+          onPressed: () async {
+            //await ref.read(myProfileVMProvider).logout();
+          },
         ),
       ],
     );
