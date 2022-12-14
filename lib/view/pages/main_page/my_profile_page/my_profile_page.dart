f584397bbd74cb2e1924f456bbe475f1e1368d75
Halil Emin Çalışkan <halileminclskn@gmail.com>
2022-12-14 18:00:56 +0300

    Fetching profile datas from current user done
    -Dump data deleted
    -Profile informations now fetching from current user
    -User class in viewmodel deleted

--- lib/view/pages/main_page/my_profile_page/my_profile_page.dart
+++ lib/view/pages/main_page/my_profile_page/my_profile_page.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:kitapp/view/pages/main_page/my_profile_page/dump_data.dart';
+import 'package:kitapp/core/global_models/public_user/public_user_model.dart';
 import 'package:kitapp/view/shared/widgets/profile.dart';
 
 import '../../../shared/styles/colors.dart';
 import '../../../shared/widgets/custom_appbar.dart';
 import '../../../shared/widgets/custom_button.dart';
 import 'viewmodel/my_profile_page_viewmodel.dart';
+import '../../../../core/init/auth_manager/auth_manager.dart';
 
 class MyProfilePage extends HookConsumerWidget {
   const MyProfilePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    const user = UserPreferences.myUser;
+    final currentUser = ref
+        .watch(authManagerProvider.select((value) => value.currentUserModel));
 
     return Scaffold(
         appBar: CustomAppbar(
@@ -27,7 +29,7 @@ class MyProfilePage extends HookConsumerWidget {
               onClicked: () async {},
             ),
             const SizedBox(height: 24),
-            buildName(user),
+            buildName(currentUser!),
             const SizedBox(height: 30),
             buildButton("My Favourite Books", "profile_favourite", context),
             const Divider(
@@ -68,17 +70,17 @@ class MyProfilePage extends HookConsumerWidget {
   }
 }
 
-Widget buildName(User user) => Column(
+Widget buildName(PublicUser user) => Column(
       children: [
         Text(
-          user.name,
+          "${user.firstName ?? "Name"} ${user.lastName ?? "Surname"}",
           style: const TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 17,
               color: Color(0xff4D506C)),
         ),
         Text(
-          user.email,
+          user.email ?? "Email",
           style: const TextStyle(fontSize: 13, color: Color(0xff4D506C)),
         ),
       ],
