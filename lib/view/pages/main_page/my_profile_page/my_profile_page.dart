fd8d5737ec2390a1f18d04ac9cb73ccea7c2017c
Yunus Kara <yunus.kara@ogr.iuc.edu.tr>
2022-12-24 14:10:12 +0300

    Profil page UI improvement

--- lib/view/pages/main_page/my_profile_page/my_profile_page.dart
+++ lib/view/pages/main_page/my_profile_page/my_profile_page.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kitapp/core/global_models/public_user/public_user_model.dart';
+import 'package:kitapp/view/shared/styles/text_styles.dart';
 import 'package:kitapp/view/shared/widgets/profile.dart';
 import 'package:auto_route/auto_route.dart';
 
 import '../../../shared/styles/colors.dart';
-import '../../../shared/widgets/custom_appbar.dart';
 import '../../../shared/widgets/custom_button.dart';
 import 'viewmodel/my_profile_page_viewmodel.dart';
 import '../../../../core/init/auth_manager/auth_manager.dart';
@@ -21,55 +21,53 @@ class MyProfilePage extends HookConsumerWidget {
         .watch(authManagerProvider.select((value) => value.currentUserModel));
 
     return Scaffold(
-        appBar: CustomAppBar(
-          title: 'My Profile Page',
-          showBackButton: false,
-        ),
-        body: Column(
-          children: [
-            ProfileWidget(
-              onClicked: () async {},
-            ),
-            const SizedBox(height: 24),
-            buildName(currentUser),
-            const SizedBox(height: 30),
-            buildButton("My Favourite Books", "profile_favourite", context),
-            const Divider(
-              color: Color(0xff4D506C),
-              indent: 20,
-              endIndent: 20,
-              thickness: 0.1,
-            ),
-            buildButton("Change Password", "profile_change_password", context,
-                isChangePassword: true),
-            Expanded(
-                child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      CustomButton(
-                        icon: SvgPicture.asset('assets/svg/profile_logout.svg'),
-                        buttonText: 'Logout',
-                        backgroundColor: AppColors.primary,
-                        height: 55,
-                        borderRadius: 11,
-                        width: MediaQuery.of(context).size.width - 40,
-                        onPressed: () async {
-                          await ref.read(myProfileVMProvider).logout();
-                        },
-                      ),
-                    ],
-                  )),
-            )),
-            const SizedBox(
-              height: 15,
-            )
-          ],
-        ));
+        body: SafeArea(
+      child: Column(
+        children: [
+          ProfileWidget(
+            onClicked: () async {},
+          ),
+          const SizedBox(height: 24),
+          buildName(currentUser),
+          const SizedBox(height: 30),
+          buildButton("My Favourite Books", "profile_favourite", context),
+          const Divider(
+            color: Color(0xff4D506C),
+            indent: 20,
+            endIndent: 20,
+            thickness: 0.1,
+          ),
+          buildButton("Change Password", "profile_change_password", context,
+              isChangePassword: true),
+          Expanded(
+              child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    CustomButton(
+                      icon: SvgPicture.asset('assets/svg/profile_logout.svg'),
+                      buttonText: 'Logout',
+                      backgroundColor: AppColors.primary,
+                      height: 55,
+                      borderRadius: 11,
+                      width: MediaQuery.of(context).size.width - 40,
+                      onPressed: () async {
+                        await ref.read(myProfileVMProvider).logout();
+                      },
+                    ),
+                  ],
+                )),
+          )),
+          const SizedBox(
+            height: 15,
+          )
+        ],
+      ),
+    ));
   }
 }
 
@@ -88,23 +86,26 @@ Widget buildName(PublicUser? user) => Column(
         ),
       ],
     );
-
 Widget buildButton(String title, String iconName, BuildContext context,
         {bool isChangePassword = false}) =>
     Column(
       mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CustomButton(
-          icon: SvgPicture.asset("assets/svg/$iconName.svg"),
-          buttonText: title,
-          backgroundColor: Colors.white,
+        ListTile(
+          leading: SvgPicture.asset(
+            "assets/svg/$iconName.svg",
+          ),
+          title: Text(
+            title,
+            style: AppTextStyles.body6.copyWith(
+              fontWeight: FontWeight.w400,
+            ),
+          ),
           textColor: const Color(0xff4D506C),
-          textAlign: TextAlign.left,
-          height: 55,
-          borderRadius: 11,
-          width: MediaQuery.of(context).size.width - 40,
-          onPressed: () async {
+          iconColor: const Color(0xff4D506C),
+          minLeadingWidth: 15,
+          onTap: () async {
             if (isChangePassword) {
               await context.router
                   .navigate(ForgotPasswordRoute(title: 'Change Password'));
